ed26acae91ddbb780eb912560e3e5fb5efb743fc
DESKTOP-VF9OCE7\allex <allexortiz@outlook.com>
2023-11-25 18:23:47 -0600

    added startScreen function

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 //Dependencies found here
+const inquirer = require('inquirer');
 const express = require('express');
-// Import and require mysql2
 const mysql = require('mysql2');
 
+// Configuration
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -12,14 +13,69 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // MySQL password
-    password: '',
-    database: 'courses_db'
-  },
-  console.log(`Connected to the courses_db database.`)
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: '',
+        database: 'employee_db'
+    },
+    console.log(`Connected to the courses_db database.`)
 );
 
+db.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + db.threadId);
+
+    startScreen();
+});
+
+// Function to display the initial user prompt
+function startScreen() {
+    inquirer
+        .prompt({
+            type: "list",
+            choices: [
+                "Add department",
+                "Add role",
+                "Add employee",
+                "View departments",
+                "View roles",
+                "View employees",
+                "Update employee role",
+                "Quit"
+            ],
+            message: "What would you like to do?",
+            name: "option"
+        })
+        .then(function (result) {
+            console.log("You entered: " + result.option);
+
+            switch (result.option) {
+                case "Add department":
+                    addDepartment();
+                    break;
+                case "Add role":
+                    addRole();
+                    break;
+                case "Add employee":
+                    addEmployee();
+                    break;
+                case "View departments":
+                    viewDepartment();
+                    break;
+                case "View roles":
+                    viewRoles();
+                    break;
+                case "View employees":
+                    viewEmployees();
+                    break;
+                case "Update employee role":
+                    updateEmployee();
+                    break;
+                default:
+                    quit();
+            }
+        });
+}
