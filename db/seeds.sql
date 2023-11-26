9c335c8835e7f1f2430f68dc5db4d0812c74bb76
DESKTOP-VF9OCE7\allex <allexortiz@outlook.com>
2023-11-25 18:35:03 -0600

    reformatted and added comments to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,26 @@
+-- Insert initial data into the 'department' table
 INSERT INTO department (name)
-VALUES ("Coaching Staff"), ("Players"), ("Football Staff"), ("Hospitality"), ("Leadership");
+VALUES
+  ("Coaching Staff"),
+  ("Players"),
+  ("Football Staff"),
+  ("Hospitality"),
+  ("Leadership");
 
+-- Insert initial data into the 'role' table
 INSERT INTO role (title, salary, department_id)
-VALUE ("Forward", 80000.00, 2), ("Steward", 6000.00, 4), ("Kit Man", 20000.00, 3), ("Manager", 500000.00, 1), ("Owner", 900000.00, 5);
+VALUES
+  ("Forward", 80000.00, 2),
+  ("Steward", 6000.00, 4),
+  ("Kit Man", 20000.00, 3),
+  ("Manager", 500000.00, 1),
+  ("Owner", 900000.00, 5);
 
+-- Insert initial data into the 'employee' table
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUE ("Dominic", "Calvert-Lewin", 1, 3), ("Farhad", "Moshiri", 5, 1), ("Sean", "Dyche", 4, 2), ("Jimmy", "Martin", 3, 2), ("Jurgen", "Klopp", 2, 2);
+VALUES
+  ("Dominic", "Calvert-Lewin", 1, 3),
+  ("Farhad", "Moshiri", 5, 1),
+  ("Sean", "Dyche", 4, 2),
+  ("Jimmy", "Martin", 3, 2),
+  ("Jurgen", "Klopp", 2, 2);
