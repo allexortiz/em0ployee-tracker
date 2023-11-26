ba6c2b359042184bf60232d91d155248baae6f1b
DESKTOP-VF9OCE7\allex <allexortiz@outlook.com>
2023-11-25 18:42:49 -0600

    defined addDepartment function

--- server.js
+++ server.js
@@ -79,3 +79,25 @@ function startScreen() {
             }
         });
 }
+
+function addDepartment() {
+
+
+    inquirer.prompt({
+
+        type: "input",
+        message: "What is the name of the department?",
+        name: "deptName"
+
+    }).then(function (answer) {
+
+
+
+        connection.query("INSERT INTO department (name) VALUES (?)", [answer.deptName], function (err, res) {
+            if (err) throw err;
+            console.table(res)
+            startScreen()
+        })
+    })
+}
+
